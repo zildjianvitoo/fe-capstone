59abee978855c8a0846436062be01b38d5f816b6
ZildjianVito <zildjianvito07@gmail.com>
2023-07-26 17:32:37 +0700

    improve hero section

--- src/components/pages/Home/Hero.js
+++ src/components/pages/Home/Hero.js
@@ -1,7 +1,7 @@
-import { Link } from 'react-router-dom';
-import restaurantFoodImage from './assets/restaurant-food.jpg';
-import './Hero.css';
-import pages from '../../../utils/pages';
+import { Link } from "react-router-dom";
+import restaurantFoodImage from "./assets/restaurant-food.jpg";
+import "./Hero.css";
+import pages from "../../../utils/pages";
 
 const Hero = () => {
   return (
@@ -11,17 +11,17 @@ const Hero = () => {
           <h1>Little Lemon</h1>
           <h2>Chicago</h2>
           <p>
-            We are a family owned Mediterranean restaurant, 
-            focused on traditional recipes served with a modern twist.
+            We are a family owned Mediterranean restaurant, focused on
+            traditional recipes served with a modern twist.
           </p>
-          <Link className="button-primary" to={pages.get('bookings').path}>
+          <Link className="button-primary" to={pages.get("bookings").path}>
             Reserve a table
           </Link>
         </div>
-        <img 
-          className="hero-image" 
-          src={restaurantFoodImage} 
-          alt="Restaurant food" 
+        <img
+          className="hero-image"
+          src={restaurantFoodImage}
+          alt="Restaurant food"
         />
       </div>
     </section>
